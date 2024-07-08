f6d7727ac3e9f132dab1597c5c0a5c6e6a13638a
Pedrooaj <pedroantonio5735@gmail.com>
2024-07-07 22:07:15 -0300

    atualizado

--- src/Services/firebase.jsx
+++ src/Services/firebase.jsx
@@ -2,16 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.FIREBASE_APP_ID,
-  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_REACT_API_URL,
+  authDomain: import.meta.env.VITE_REACT_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_REACT_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_REACT_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_REACT_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_REACT_FIREBASE_MEASUREMENT_ID,
 };
 
-
+console.log(firebaseConfig); 
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
